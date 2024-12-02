5420bb9ff329736f1d2ccbcb288bcdc8da6abd9e
AyezaNaseem <ayezanaseem4@gmail.com>
2024-12-02 12:17:22 +0500

    new file

--- sum-pos.asm
+++ sum-pos.asm
@@ -1,18 +1,38 @@
-include irvine32.inc
+INCLUDE Irvine32.inc  
+
 .data
-
-; array and other variables declaration here
-
+  myArray word 2,-3,5,6,-9 
+  msgSum byte "Sum of Positive Numbers : " ,0 
+  msgCount byte "Total Positive Numbers: " ,0
+  sum dword 0
+  count dword 0 
 .code
-main proc
+main PROC
+  mov eax , 0
+  mov esi , OFFSET myArray
+  mov ecx,lengthof myArray
+PositiveNumbers: 
+  mov ax , word ptr[esi]
+  test ax,8000h
+  jnz skipPositive
+  movsx eax,word ptr [esi]
+  add sum,eax 
+  add count , 1 
+skipPositive:
+  add esi,type myArray
+  loop PositiveNumbers 
+  mov edx ,OFFSET msgSum
+  call writeString 
+  call crlf
+  mov eax , sum
+  call writeint 
+  call crlf
+  mov edx , OFFSET msgCount
+  call writeString
+  call crlf
+  mov eax , count
+  call writeint
+  exit
 
-
-;code here
-
-
-
-exit
-main endp
-
-
-end main
+main ENDP
+end main 
